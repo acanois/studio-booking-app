82cc228f8055989e11c3aeb8757b1b47d04b944c
acanois <david.richter42@gmail.com>
2021-06-24 15:26:45 -0500

    stalemate

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import User
+
 def index(req):
     return render(req, 'index.html')
 
+def get_all(req):
+    return render(req, 'all_data.html')
+
+# def index(req):
+#     return HttpResponse(User.objects.all())
--- studio/settings.py
+++ studio/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 # Application definition
 
 INSTALLED_APPS = [
+    'booking.apps.BookingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking.apps.BookingConfig',
 ]
 
 MIDDLEWARE = [
