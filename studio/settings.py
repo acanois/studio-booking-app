6a472ebf1f2ce499f49d97105ead3e1170eaa089
acanois <david.richter42@gmail.com>
2021-06-30 12:18:55 -0500

    add vue config and modify settings_dot_py

--- studio/settings.py
+++ studio/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'booking', 'templates'),
+            os.path.join(BASE_DIR, 'dist'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -131,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Place static in the same location as webpack build files
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist', 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
